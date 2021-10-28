60f01895cf81edc38220ca46a35c763d974c4f5a
Ho3einzargar <Ho3einzargar@yahoo.com>
2021-10-28 07:17:33 +0330

    fetch problem 2

--- js/test.js
+++ js/test.js
@@ -2,11 +2,33 @@
 
 ///////new
 
+
+try {
 alert("hey");
-let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+let url = 'Ho3einz';
 
 let response = await fetch(url);
 
 let commits = await response.json(); 
 
 alert(commits[0].author.login);
+}
+catch (err){
+    alert(err);
+
+}
+
+// async function userinfo() {
+//     let url = `https://api.github.com/users/${user}`;
+//     try {
+//         let result = await fetch(url);
+//         let userInfo =  await result.json();
+//         document.write(`<h3> Login: ${userInfo.login}</h3> <br> 
+//                          <strong> Site Admin:</strong> ${userInfo.id}<br> 
+//                          <strong> Node Id: </strong> ${userInfo.node_id}`) ;
+//      } catch (err) {
+//     console.log(err);
+//     }
+//     }
+ 
+//    userinfo();
